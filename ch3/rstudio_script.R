d89be7247168bf5bd2c30a897a7e3c8b1a787c34
Coeat.k <justkukaro@naver.com>
2022-03-09 22:31:24 +0900

    feat: finish review

--- ch3/rstudio_script.R
+++ ch3/rstudio_script.R
@@ -1,6 +1,14 @@
 market <- read.table('./market-1.txt', header=T)
 market.lm <- lm(Y ~ X, data = market)
+market.lm.summary <- summary(market.lm)
 plot(market$X, market$Y, xlab='Advertising Fee', ylab='Total Sales', pch=19)
 title("Scatterplot of Advertising Fees and Sales ")
 abline(market.lm)
 identify(market$X, market$Y)
+β0 <- market.lm.summary$coefficients[2, 1]
+β1 <- market.lm.summary$coefficients[1, 1]
+xbar <- mean(market$X)
+ybar <- mean(market$Y)
+text(xbar, ybar, paste0('(', xbar, ',', ybar, ')'))
+fx <- paste0('Ŷ = ',β0,' + ',β1,'*X')
+text(locator(1), fx)
--- ch3/test.R
+++ ch3/test.R
@@ -1,8 +1,15 @@
 market <- read.csv('./ch2/ads.txt')
 market.lm <- lm(Y ~ X, data = market)
-data <- summary(market.lm)
-data$coefficients[2,1]
+market.lm.summary <- summary(market.lm)
+market.lm.anova <- anova(market.lm)
 plot(market$X, market$Y, xlab='Advertising Fee', ylab='Total Sales', pch=19)
 title("Scatterplot of Advertising Fees and Sales ")
 abline(market.lm)
 identify(market$X, market$Y)
+β0 <- market.lm.summary$coefficients[2, 1]
+β1 <- market.lm.summary$coefficients[1, 1]
+xbar <- mean(market$X)
+ybar <- mean(market$Y)
+text(xbar, ybar, paste0('(', xbar, ',', ybar, ')'))
+fx <- paste0('Ŷ = ',β0,' + ',β1,'*X')
+text(locator(1), fx)
